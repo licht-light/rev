3fa8754168279449ec2a5e29201f846b1b702de6
Mardhex <74248551+kmardhex@users.noreply.github.com>
2022-10-26 21:37:17 +0700

    Update add-ws.sh

--- xcore/add-ws.sh
+++ xcore/add-ws.sh
@@ -35,7 +35,7 @@ cat > /etc/v2ray/$user-tls.json <<-EOF
       "v": "2",
       "ps": "${user}",
       "add": ".mdxstore.my.id",
-      "port": "8443",
+      "port": "443",
       "id": "${uuid}",
       "aid": "0",
       "net": "ws",
@@ -51,7 +51,7 @@ cat > /etc/v2ray/$user-none.json <<-EOF
       "v": "2",
       "ps": "${user}",
       "add": "vip.mdxstore.my.id",
-      "port": "8080",
+      "port": "80",
       "id": "${uuid}",
       "aid": "0",
       "net": "ws",
@@ -72,8 +72,8 @@ echo -e "==========-V2RAY/VMESS-=========="
 echo -e "Remarks        : ${user}"
 echo -e "Domain         : ${domain}"
 echo -e "Server IP      : $PUBLIC_IP"
-echo -e "port TLS       : 8443"
-echo -e "port none TLS  : 8080"
+echo -e "port TLS       : 443"
+echo -e "port none TLS  : 80"
 echo -e "id             : ${uuid}"
 echo -e "alterId        : 0"
 echo -e "Security       : auto"
