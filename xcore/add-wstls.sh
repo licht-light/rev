9528466e249c86d924cfac0a39d8d1ff015fe94e
Mardhex <74248551+kmardhex@users.noreply.github.com>
2022-11-02 06:29:24 +0700

    Update add-wstls.sh

--- xcore/add-wstls.sh
+++ xcore/add-wstls.sh
@@ -40,7 +40,7 @@ cat>/etc/v2ray/$user-tls.json<<EOF
       "id": "${uuid}",
       "aid": "0",
       "net": "ws",
-      "path": "/kuota-habis",
+      "path": "/MDXCloud",
       "type": "none",
       "host": "",
       "tls": "tls"
@@ -63,7 +63,7 @@ echo -e "id             : ${uuid}"
 echo -e "alterId        : 0"
 echo -e "Security       : auto"
 echo -e "network        : ws"
-echo -e "path           : /kuota-habis"
+echo -e "path           : /MDXCloud"
 echo -e "================================="
 echo -e "link TLS       : ${vmesslink1}"
 echo -e "================================="
