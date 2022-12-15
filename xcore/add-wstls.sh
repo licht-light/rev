c423b3f750bfbdae8940cd655c0db999f9682b7f
Yayang Irawan <74248551+kmardhex@users.noreply.github.com>
2022-12-15 11:05:32 +0700

    Update add-wstls.sh

--- xcore/add-wstls.sh
+++ xcore/add-wstls.sh
@@ -40,7 +40,7 @@ cat>/etc/v2ray/$user-tls.json<<EOF
       "id": "${uuid}",
       "aid": "0",
       "net": "ws",
-      "path": "/portalWeb/mainlogin.do",
+      "path": "/worryfree",
       "type": "none",
       "host": "${domain}",
       "tls": "tls"
@@ -63,7 +63,7 @@ echo -e "id             : ${uuid}"
 echo -e "alterId        : 0"
 echo -e "Security       : auto"
 echo -e "network        : ws"
-echo -e "path           : /portalWeb/mainlogin.do"
+echo -e "path           : /worryfree"
 echo -e "================================="
 echo -e "link TLS       : ${vmesslink1}"
 echo -e "================================="
