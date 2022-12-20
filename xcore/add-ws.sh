2b7c427580a9a331dc5927799b97fafc62936c80
Yayang Irawan <74248551+kmardhex@users.noreply.github.com>
2022-12-20 13:17:46 +0700

    Update add-ws.sh

--- xcore/add-ws.sh
+++ xcore/add-ws.sh
@@ -39,7 +39,7 @@ cat > /etc/v2ray/$user-tls.json <<-EOF
       "id": "${uuid}",
       "aid": "0",
       "net": "ws",
-      "path": "/worryfree",
+      "path": "/mdxstore",
       "type": "none",
       "host": "${domain}",
       "tls": "tls"
@@ -55,7 +55,7 @@ cat > /etc/v2ray/$user-none.json <<-EOF
       "id": "${uuid}",
       "aid": "0",
       "net": "ws",
-      "path": "/worryfree",
+      "path": "/mdxstore",
       "type": "none",
       "host": "${domain}",
       "tls": "none"
@@ -78,7 +78,7 @@ echo -e "id             : ${uuid}"
 echo -e "alterId        : 0"
 echo -e "Security       : auto"
 echo -e "network        : ws"
-echo -e "path           : /worryfree"
+echo -e "path           : /mdxstore"
 echo -e "================================="
 echo -e "link TLS       : ${vmesslink1}"
 echo -e "================================="
