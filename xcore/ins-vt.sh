30762d13ea174fcc2e567e386bb860919d1cf8ee
Yayang Irawan <74248551+kmardhex@users.noreply.github.com>
2023-02-09 21:39:28 +0700

    Update ins-vt.sh

--- xcore/ins-vt.sh
+++ xcore/ins-vt.sh
@@ -42,7 +42,7 @@ cat > /etc/v2ray/config.json <<-EOF
   },
   "inbounds": [
     {
-      "port": 443,
+      "port": 8443,
       "protocol": "vmess",
       "settings": {
         "clients": [
@@ -65,7 +65,7 @@ cat > /etc/v2ray/config.json <<-EOF
           ]
         },
         "wsSettings": {
-          "path": "/worryfree",
+          "path": "/chat",
           "headers": {
             "Host": ""
           }
@@ -153,7 +153,7 @@ cat > /etc/v2ray/none.json <<-EOF
       "streamSettings": {
         "network": "ws",
         "wsSettings": {
-          "path": "/worryfree",
+          "path": "/chat",
           "headers": {
             "Host": ""
           }
@@ -251,7 +251,7 @@ cat > /etc/v2ray/vless.json <<-EOF
           ]
         },
         "wsSettings": {
-          "path": "/worryfree",
+          "path": "/chat",
           "headers": {
             "Host": ""
           }
@@ -339,7 +339,7 @@ cat > /etc/v2ray/vnone.json <<-EOF
       "streamSettings": {
         "network": "ws",
         "wsSettings": {
-          "path": "/worryfree",
+          "path": "/chat",
           "headers": {
             "Host": ""
           }
@@ -409,7 +409,7 @@ cat > /etc/trojan/config.json <<-EOF
 {
     "run_type": "server",
     "local_addr": "0.0.0.0",
-    "local_port": 8443,
+    "local_port": 443,
     "remote_addr": "127.0.0.1",
     "remote_port": 2603,
     "password": [
