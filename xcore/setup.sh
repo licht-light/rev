0bf6f6d5d24476b384576a1e7440d68a26b44406
Yayang Irawan <74248551+kmardhex@users.noreply.github.com>
2023-02-22 16:21:00 +0700

    Update setup.sh

--- xcore/setup.sh
+++ xcore/setup.sh
@@ -107,7 +107,7 @@ echo "   - Nginx                   : 81"  | tee -a log-install.txt
 echo "   - Wireguard               : 7070"  | tee -a log-install.txt
 echo "   - L2TP/IPSEC VPN          : 1701"  | tee -a log-install.txt
 echo "   - PPTP VPN                : 1732"  | tee -a log-install.txt
-echo "   - SSTP VPN                : 444"  | tee -a log-install.txt
+echo "   - SSTP VPN                : 5555"  | tee -a log-install.txt
 echo "   - Shadowsocks-R           : 1443-1543"  | tee -a log-install.txt
 echo "   - SS-OBFS TLS             : 2443-2543"  | tee -a log-install.txt
 echo "   - SS-OBFS HTTP            : 3443-3543"  | tee -a log-install.txt
